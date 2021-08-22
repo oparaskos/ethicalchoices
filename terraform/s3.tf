a4c2035423e78d08f33156c7ef1c02bec4bbe993
Oliver Paraskos <oparaskos@googlemail.com>
2021-08-22 23:46:46 +0100

    fix code advisories

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,6 +6,8 @@ resource "aws_s3_bucket_public_access_block" "block-public-access" {
 	bucket = aws_s3_bucket.site.id
 	block_public_acls   = true
 	block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
